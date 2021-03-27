2560dbf47a08952226ee8dd06f893189824a6d5d
Vishwas <vishwasvb1997@gmail.com>
2021-03-27 14:03:49 +0530

    modified commit

--- movieapp/src/components/movies.jsx
+++ movieapp/src/components/movies.jsx
@@ -1,10 +1,17 @@
 import React, {Component} from 'react'
 
 class Movies extends Component {
-    state = { }
+    state = {
+        count: 0
+     }
     render () {
+        let name='Vishwas';
         return (
-            <h1>Movie List</h1>
+            // <h1>Movie List</h1> normal h1 tag
+            // <h1>{2*2}</h1> expression
+            // <h1>{name} </h1> variables
+            // <h1>{this.state.count}</h1> object value
+             <h1>{`My Name is ${name}`} </h1> // using back tick
         );
     }
 }
