bed55311a5e8e0e762addf83edfdf1e4854460dc
Vishwas <vishwasvb1997@gmail.com>
2021-03-27 14:46:22 +0530

    adding values to table through map()

--- movieapp/src/components/movies.jsx
+++ movieapp/src/components/movies.jsx
@@ -1,8 +1,10 @@
 import React, {Component} from 'react'
+import {getMovies} from '../services/fakeMovieService';
 
 class Movies extends Component {
     state = {
-        count: 0
+        // count: 0
+        movies: getMovies()
      }
     render () {
         let name='Vishwas';
@@ -11,9 +13,31 @@ class Movies extends Component {
             // <h1>{2*2}</h1> expression
             // <h1>{name} </h1> variables
             // <h1>{this.state.count}</h1> object value
-             <h1>{`My Name is ${name}`} </h1> // using back tick
-
+            //  <h1>{`My Name is ${name}`} </h1> // using back tick
             // <//h1></h1>
+
+
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Genre</th>
+                        <th>Stock</th>
+                        <th>Rate</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.state.movies.map(movie => (
+                        <tr>
+                        <td>{movie.title}</td>
+                        <td>{movie.genre.name}</td>
+                        <td>{movie.numberInStock}</td>
+                        <td>{movie.dailyRentalRate}</td>
+                    </tr>
+                    ))}
+                    
+                </tbody>
+            </table>
         );
     }
 }
