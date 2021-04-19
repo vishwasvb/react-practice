e4f486abf8190bd950bcefe215b0898ff2ae1b44
Vishwas <vishwasvb1997@gmail.com>
2021-04-19 14:31:52 +0530

    Functional components

--- counter-app/src/component/navbar.jsx
+++ counter-app/src/component/navbar.jsx
@@ -1,15 +1,31 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 
-class NavBar extends Component{
-    render(){
-        return(
-            <nav class="navbar navbar-light bg-light">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">{this.props.totalCounters}</a>
-                </div>
-            </nav>
-        );
-    }
+const NavBar = (props) => {
+    return ( 
+        <nav class="navbar navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Total Count
+                <span class="badge badge-primary">{props.totalCounters}</span></a>
+            </div>
+        </nav>
+     );
 }
+ 
+// export default ;
+
+
+
+// class NavBar extends Component{
+//     render(){
+//         return(
+//             <nav class="navbar navbar-light bg-light">
+//                 <div class="container-fluid">
+//                     <a class="navbar-brand" href="#">Total Count
+//                     <span class="badge badge-primary">{this.props.totalCounters}</span></a>
+//                 </div>
+//             </nav>
+//         );
+//     }
+// }
 
 export default NavBar;
