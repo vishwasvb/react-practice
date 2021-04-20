89ece8a3d57be38f2b0e266b4aad50a2bc982175
Vishwas <vishwasvb1997@gmail.com>
2021-04-20 13:45:01 +0530

    converting Like class component to Like functional component

--- movieapp/src/components/like.jsx
+++ movieapp/src/components/like.jsx
@@ -1,18 +1,30 @@
 import React, {Component} from 'react'
 
-class Like extends Component{
-    render() {
-        let classes = 'fa fa-heart';
-        if(!this.props.likeStatus){
-            classes+='-o';
-        }
-        else{
-            
-        }
-    return (
-            <i className={classes} onClick={this.props.onLike} style={{cursor:"pointer"}}></i>
-        );
+const Like = (props) => {
+    let classes = 'fa fa-heart';
+    if(!props.likeStatus){
+        classes+='-o';
     }
+    return ( 
+        <i className={classes} onClick={props.onLike} style={{cursor:"pointer"}}></i>
+     );
 }
-       
+ 
 export default Like;
+
+//props/ class Like extends Component{
+//     render() {
+//         let classes = 'fa fa-heart';
+//         if(!this.props.likeStatus){
+//             classes+='-o';
+//         }
+//         else{
+            
+//         }
+//     return (
+//             <i className={classes} onClick={this.props.onLike} style={{cursor:"pointer"}}></i>
+//         );
+//     }
+// }
+       
+// export default Like;
