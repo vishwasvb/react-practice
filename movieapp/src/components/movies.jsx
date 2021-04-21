42249a6348e33944cdfb42dd9eef19c8c4e1c53d
Vishwas <vishwasvb1997@gmail.com>
2021-04-21 17:13:25 +0530

    Sorting part-4 (sorting using all the attributes in both the orders)

--- movieapp/src/components/movies.jsx
+++ movieapp/src/components/movies.jsx
@@ -27,6 +27,16 @@ class Movies extends Component {
 
     handleSort = (path) =>{
         // console.log(path);
+        const sortColumn = {...this.state.sortColumn};
+        if(sortColumn.path === path)
+            sortColumn.order = (sortColumn.order === 'asc')? 'desc' : 'asc';
+        else{
+            sortColumn.path=path;
+            sortColumn.order='asc';
+        }
+        this.setState({
+            sortColumn
+        });
 
     }
 
