3ee7954d74371de81bb5e9da4bb5f902c46c66f7
Vishwas <vishwasvb1997@gmail.com>
2021-04-26 10:26:57 +0530

    reference object of the elements(focus and .value

--- movieapp/src/components/loginForm.jsx
+++ movieapp/src/components/loginForm.jsx
@@ -2,9 +2,19 @@ import React, {Component} from 'react'
 
 class LoginForm extends Component {
     
+    username = React.createRef();//this will create a ref object
+
     handleSubmit = obj =>{
         obj.preventDefault();
         console.log("Form Submitted");
+        
+        // const username=document.getElementById('username').value; //this should not be used in react we should only work on virtual DOM, we Should not directly access real DOM
+        const username=this.username.current.value;
+        console.log(username); 
+    }
+
+    componentDidMount() {
+        this.username.current.focus();
     }
 
     render() { 
@@ -14,7 +24,7 @@ class LoginForm extends Component {
                 <form onSubmit={this.handleSubmit}>
                     <div className="form-group">
                         <lable htmlFor="username">User Name</lable>
-                        <input type="text" id="username" className="form-control"/>
+                        <input ref={this.username} type="text" id="username" className="form-control"/>
                     </div>
                     <div className="form-group">
                         <lable htmlFor="password">Password</lable>
