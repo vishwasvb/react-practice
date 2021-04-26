547b8125f2517ed4c4e956deb501f69ee3a19dd7
Vishwas <vishwasvb1997@gmail.com>
2021-04-26 10:57:36 +0530

    handleChange and setting the state

--- movieapp/src/components/loginForm.jsx
+++ movieapp/src/components/loginForm.jsx
@@ -2,20 +2,31 @@ import React, {Component} from 'react'
 
 class LoginForm extends Component {
     
-    username = React.createRef();//this will create a ref object
+    // username = React.createRef();//this will create a ref object
+
+    state ={
+        account: {username:'',password:''}
+    }
 
     handleSubmit = obj =>{
         obj.preventDefault();
         console.log("Form Submitted");
+        console.log(this.state.account);
         
         // const username=document.getElementById('username').value; //this should not be used in react we should only work on virtual DOM, we Should not directly access real DOM
-        const username=this.username.current.value;
-        console.log(username); 
+        // const username=this.username.current.value;
+        // console.log(username); 
     }
 
-    componentDidMount() {
-        this.username.current.focus();
+    handleChange = obj => {
+        const account = {...this.state.account};
+        account.username = obj.currentTarget.value;
+        this.setState({account});
     }
+
+    // componentDidMount() {
+    //     this.username.current.focus();
+    // }
 
     render() { 
         return ( 
@@ -24,7 +35,8 @@ class LoginForm extends Component {
                 <form onSubmit={this.handleSubmit}>
                     <div className="form-group">
                         <lable htmlFor="username">User Name</lable>
-                        <input ref={this.username} type="text" id="username" className="form-control"/>
+                        {/* <input ref={this.username} type="text" id="username" className="form-control"/> */}
+                        <input autoFocus onChange={this.handleChange} type="text" id="username" className="form-control"/>
                     </div>
                     <div className="form-group">
                         <lable htmlFor="password">Password</lable>
